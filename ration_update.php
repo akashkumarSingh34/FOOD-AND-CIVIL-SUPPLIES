082feabddbea4f1b61a44b16177642d317e0672e
Akash Kumar Singh <83223139+akashkumarSingh34@users.noreply.github.com>
2021-12-16 21:24:31 +0530

    Update ration_update.php

--- ration_update.php
+++ ration_update.php
@@ -210,10 +210,7 @@ if (isset($_POST["submit"])) {
                         </ul>
                     </li>
 
-                    <!-- Contact Us -->
-                    <li class="nav-item">
-                        <a class="nav-link" style="margin-right: 10px;" href="#">Contact Us</a>
-                    </li>
+
 
                     <!-- LogOut -->
                     <li class="nav-item">
